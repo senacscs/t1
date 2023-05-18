13ae8c003d0dbaa57e890dfd82cf04b6381f7fca
analuhhka <105052528+analuhhka@users.noreply.github.com>
2023-05-18 09:11:59 -0300

    Add files via upload

---
+++ ana/ana.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+
+<html lang = "pt-br">
+
+    <head>
+
+        <title> Exemplo</title>
+
+        <meta charset = "UTF-8">
+
+    </head>
+
+    <body>
+
+    <?php
+
+    /*
+comentário de
+multiplas
+linhas
+*/
+
+echo "/* */ é usado para comentário de múltiplas linhas, em quanto // é usado pra comentários de uma linha";
+
+//comentário de uma linha
+
+        ?>      
+
+    </body>
+
+</html>
