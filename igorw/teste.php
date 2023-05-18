7ca289e689fdafb1ea8eeaa73e883e0b3d3e5e69
w1ck3rt <105052709+w1ck3rt@users.noreply.github.com>
2023-05-18 08:14:28 -0300

    Add files via upload

---
+++ igorw/teste.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+
+<html lang = "pt-br">
+
+    <head>
+
+        <title>Teste</title>
+        <meta charset = "UTF-8">
+
+    </head>
+
+    <style>
+
+    html{
+        text-align:center;
+    }
+
+    </style>
+
+    <body>
+
+    <?php
+$agora = date('d/m/y');
+echo $agora;
+    ?><br>
+
+    <?php
+$agora = date('H:i');
+echo $agora;
+    ?>
+    
+    </body>
+
+</html>
