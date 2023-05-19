7580b108c4fcbc24f60dd59b3d991d5d24f783fb
Igoreg300 <110827070+Igoreg300@users.noreply.github.com>
2023-05-19 08:48:13 -0300

    Add files via upload

---
+++ dudu/Java/index.js
@@ -0,0 +1,11 @@
+// https://dribbble.com/shots/2955075-Neon-Sign-in-CSS
+
+window.addEventListener("load", function() {
+    var fadeContainer = document.querySelector("#fade-container");
+  
+  setTimeout(function() {
+      
+        fadeContainer.style.display = "none";
+    
+  }, 2000);
+});
