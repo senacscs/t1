43d52405a8c96ddd430af1c849d7d18007bfb04c
Igoreg300 <110827070+Igoreg300@users.noreply.github.com>
2023-05-19 08:44:43 -0300

    Add files via upload

---
+++ dudu/escape_room/mecanica.js
@@ -0,0 +1,3 @@
+
+for (i=0;i<1;i++)//i++ é igual a i=i=1
+    alert ("Em baixo do balde havia um nome de um antigo prisioneiro. Pode ser uma boa procurar por algo que ele possa ter deixado para traz.");
