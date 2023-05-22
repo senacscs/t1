a21cca14a71753dda7a9ff17494ef742974fa5a4
feltinho <110824041+feltinho@users.noreply.github.com>
2023-05-22 12:12:13 -0300

    Add files via upload

---
+++ felten/crud/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Sample file: Never send your credentials to git
+
+// host
+$host = 'http://localhost:8080/crud';
+
+// db
+$db_name = 'crud';
+$db_host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+  $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+} catch (\Throwable $th) {
+  throw $th;
+}
