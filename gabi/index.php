e944f2895ec5c531bd0b5390d0aafae32a683b38
gaabiimachado <105052766+gaabiimachado@users.noreply.github.com>
2023-05-22 12:15:11 -0300

    Add files via upload

---
+++ gabi/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html lang = "pt-br">
+
+<head>
+
+        <title> Exemplo</title>
+
+        <meta charset = "UTF-8">
+
+    </head>
+
+    <body>
+<p> oi (html) <p> 
+
+
+
+    <?php
+    $name = "Guilherme";
+    $age = 20;
+    $ola = "bom dia";
+  
+    echo $name; // Guilherme
+    echo "</br>";
+    echo $age; // 20
+    echo "<br>";
+    echo $ola; 
+    echo "<br>";
+    echo "oi";
+
+        ?>      
+
+    </body>
+
+</html>
