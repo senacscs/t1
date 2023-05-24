369b6850904472993ee8cb6e9bc48202f704b46e
Luiz Henrique Rauber Rodrigues <133702602+luizrauber@users.noreply.github.com>
2023-05-24 07:44:03 -0300

    poo em luiz

--- luiz/script.js
+++ luiz/script.js
@@ -1,46 +1,64 @@
 class Animal {
-constructor(name, patas) {
-    this.name = name;
-    this.patas = patas;
-}
-
-correr() {
-    document.getElementById("mostrar").innerHTML = this.name + " está correndo";
-    console.log(`${this.name} está correndo`);
-}
+    constructor(nome, patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+    
+    correr() {
+        document.getElementById("mostrar").innerHTML = this.nome + " está correndo";
+        console.log(`${this.nome} está correndo`);
+    }
 }
 
 class Cachorro extends Animal {
-constructor(nome) {
-    super(nome, 4);
-}
+    constructor(nome) {
+        super(nome, 4);
+    }
 }
 
 class Humano extends Animal {
-constructor(nome) {
-    super(nome, 2);
+    constructor(nome) {
+        super(nome, 2);
+    }
 }
+
+class Cadeira {
+    constructor(nome, patas) {
+        this.nome = nome;
+        this.patas = patas;
+    }
+
+    correr() {
+        document.getElementById("mostrar").innerHTML = this.nome + " é uma cadeira, não pode correr";
+        console.log("${this.nome} é uma cadeira, não pode correr");
+    }
 }
 
 function criaObjeto() {
     let nome = document.getElementById("nomeInput").value;
     let patas = parseInt(document.getElementById("patasInput").value);
 
-if (patas === 2) {
-    return new Humano(nome);
-} else if (patas === 4) {
-    return new Cachorro(nome);
-} else {
-    return null;
-}
+    if (patas === 2) {
+        return new Humano(nome);
+    } else if (patas === 4) {
+        let respira = prompt("Respira?");
+        if (respira === "sim" || respira === "Sim" || respira === "SIM" || respira === "s" || respira === "S") {
+            return new Cachorro(nome);
+        }
+        return new Cadeira(nome);
+    } else if (patas === 3) {
+        return new Cadeira(nome);
+    } else {
+        return null;
+    }
 }
 
-function criarAnimal() {
-let obj = criaObjeto();
-if (obj === null) {
-    console.log(`Para patas é 2 ou 4 apenas`);
-} else {
-    obj.correr();
-}
+function criar() {
+    let obj = criaObjeto();
+    if (obj === null) {
+        console.log("Para patas é 2 ou 4 apenas");
+    } else {
+        obj.correr();
+    }
 }
 
