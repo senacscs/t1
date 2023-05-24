04d47b5d2ff52dc80efd1d3ebf27866c43383a05
w1ck3rt <105052709+w1ck3rt@users.noreply.github.com>
2023-05-24 08:17:09 -0300

    Delete igorw/anuario directory

--- igorw/anuario/testes/login.php
+++
@@ -1,5 +0,0 @@
-<form method="post" action="../sobre/index.html">
-    <input type="text" name="login">
-    <input type="password" name="senha">
-    <input type="submit" name="acao" value="Enviar">
-</form>
